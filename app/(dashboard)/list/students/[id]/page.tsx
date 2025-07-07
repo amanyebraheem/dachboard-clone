f6bb0f940d3e8ce50d9f8ac6c97ee7bf79d10085
amanyebraheem <amanyebraheem9900@gmail.com>
2025-07-07 05:11:13 +0300

    first commit

--- app/(dashboard)/list/students/[id]/page.tsx
+++ app/(dashboard)/list/students/[id]/page.tsx
@@ -1,6 +1,6 @@
-import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalender";
-import Performance from "@/components/Performance";
+import Announcements from "../../../../components/Announcements";
+import BigCalendar from "../../../../components/BigCalender";
+import Performance from "../../../../components/Performance";
 import Image from "next/image";
 import Link from "next/link";
 
